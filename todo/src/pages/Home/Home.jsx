da05add7613ceb708b15289432ad245159d5e0ae
Nikita Gupta <nikitagupta1709@gmail.com>
2022-12-13 17:37:55 +0530

    created task

--- todo/src/pages/Home/Home.jsx
+++ todo/src/pages/Home/Home.jsx
@@ -3,7 +3,19 @@ import './home.css'
 export const Home = () => {
   return (
     <div className='home'>
-        Home
+        <h1>Create a new task</h1>
+        <form >
+                <input type="text" placeholder="Enter Task Name" required/>
+                <input type="date" required /><br />
+                <textarea rows="8" cols="50" placeholder="Task Description..." required/><br />
+                <select  id="" placeholder="Select"  required>
+                    <option value="">Select Task Category</option>
+                    <option value="personal">Personal</option>
+                    <option value="official">Official</option>
+                    <option value="others">Others</option>
+                </select>
+                <button type="submit" className="createTaskBtn">Create Task</button>
+            </form>
     </div>
   )
 }
